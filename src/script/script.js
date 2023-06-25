70bb590c31100cdc8076b1384a584b322e063341
Anaïs Méziane <anais_meziane@icloud.com>
2023-06-25 14:58:58 +0200

    burger menu

--- src/script/script.js
+++ src/script/script.js
@@ -1,15 +1,15 @@
-if (window.matchMedia("(min-width: 430px)").matches) {
-    document.addEventListener("scroll", function () {
-        const anchoredCtaWeb = document.getElementById("header-change");
-        if (window.pageYOffset > 4) {
-            anchoredCtaWeb.style.minHeight = "70px";
-            anchoredCtaWeb.classList.add("sticky");
-        }
-        if (window.pageYOffset < 3) {
-            anchoredCtaWeb.classList.remove("sticky");
-        }
-    });
-}
+// if (window.matchMedia("(min-width: 430px)").matches) {
+document.addEventListener("scroll", function () {
+    const anchoredCtaWeb = document.getElementById("header-change");
+    if (window.pageYOffset > 4) {
+        anchoredCtaWeb.style.minHeight = "70px";
+        anchoredCtaWeb.classList.add("sticky");
+    }
+    if (window.pageYOffset < 3) {
+        anchoredCtaWeb.classList.remove("sticky");
+    }
+});
+// }
 
 function setActive(element) {
     // Supprimer la classe active de tous les éléments du menu
@@ -18,4 +18,16 @@ function setActive(element) {
 
     // Ajouter la classe active à l'élément sélectionné
     element.classList.add('active');
+}
+
+if (window.matchMedia("(max-width: 430px)").matches) {
+    const hamburgerButton = document.querySelector(".nav-toggler")
+    const navigation = document.querySelector(".items")
+
+    hamburgerButton.addEventListener("click", toggleNav)
+
+    function toggleNav() {
+        hamburgerButton.classList.toggle("active")
+        navigation.classList.toggle("active")
+    }
 }
